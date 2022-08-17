4bd5e2363efe9b8139aae1734725488040f34ce5
tsear <98225311+tsear@users.noreply.github.com>
2022-08-17 17:07:42 -0400

    Update

--- home.py
+++ home.py
@@ -1,14 +1,21 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame()
 
+#creates two containers
 header = st.container()
 body = st.container()
 
 with header:
-    st.title('NFT Car Marketplace')
+    st.title('MotorChain')
 
+with header:
+    st.markdown("""---""") 
+
+with header:
+    st.subheader('The premiere Blockchain car dealership!')
+
+# creates all user input prompts
 with body:
     st.text_input(
         label='Owner Name',
@@ -29,7 +36,7 @@ with body:
     st.text_input(
         label='Year',
         value='',
-        max_chars=50,
+        max_chars=4,
         key=None,
         autocomplete=None
     )
@@ -40,21 +47,43 @@ with body:
     st.text_input(
         label='VIN Number',
         value='',
-        max_chars=50,
+        max_chars=17,
         key=None,
         autocomplete=None
     )
     st.text_input(
         label='Description of Vehicle',
         value='',
-        max_chars=50,
+        max_chars=200,
         key=None,
         autocomplete=None
-
+    )
+    st.file_uploader(
+        label='Input Vehicle Photo',
+        type=['png', 'jpg'],
+        accept_multiple_files=True,
+        help='Input .jpg or .png of the vehicle you would like to list.'
     )
 
+#Sidebar buttons (not yet functional)
     with header:
         st.sidebar.button(
             label='Connect Wallet',
             on_click=None
         )
+    with body:
+        st.sidebar.button(
+            label='Marketplace',
+            on_click=None
+        )
+        st.sidebar.button(
+            label='Documentation',
+            on_click=None
+        )
+
+    with body:
+        if st.button('List Vehicle'):
+            st.write('Listing!')
+    
+st.markdown("""---""") 
+
