64ae0bdebc8669d6b94a6eb1d2f4003c0ae6cc4f
Scott Marler <scottjmarler@gmail.com>
2022-08-22 12:03:19 -0400

    Updated auto stocks with cache decorator

--- yfinance_streamlit.py
+++ yfinance_streamlit.py
@@ -1,56 +1,85 @@
-"""
-
-Cryptocurency Daily Prices Webapp
-
-- Using streamlit and Yahoo Finance
-
-
-"""
-
-from curses import use_default_colors
 import yfinance as yf  # Yahoo finance to get stock data
-import streamlit as st  # Streamlit to create the webapp
-from PIL import Image  # Import Pillow to add icons
-from urllib.request import urlopen  # To add URLS
+import streamlit as st  # Streamlit to create the webapp 
 import pandas as pd
+from datetime import datetime
+st.set_page_config(layout="wide", initial_sidebar_state="expanded")
+
+      
+# ------ layout setting---------------------------
+stock_selector = st.sidebar.container() 
+stock_selector.markdown("## Auto Stocks")
+
+# ---------Selectbox for users to pick a stock to evaluate-----------------
+
+stock_picks = ("General Motors","Tesla","VW","Toyota","Honda","Ford","Stellantis","Hyundai","Volvo","Audi","BMW","Mercedes")  
+ticker = stock_selector.selectbox("Pick a Stock",stock_picks)
+
+# ---------Declare Ticker Symbols Based on User Selection-----------------
+
+GM = 'GM'
+Tesla = 'TSLA'  
+Vwagon = 'VWAGY'
+Toyota = 'TM'
+Honda = 'HMC'
+Ford = 'F'
+Stellantis = 'STLA'
+Hyundai = 'HYMTF'
+Volvo = 'VLVLY'
+Audi = 'AUDVF'
+BMW = 'BMWYY'
+Mercedes = 'DMLRY'
+
+if ticker == "General Motors":
+    ticker = GM 
+if ticker == "Tesla":
+    ticker = Tesla
+if ticker == "VW":
+    ticker = Vwagon
+if ticker == "Toyota":
+    ticker = Toyota
+if ticker == "Honda":
+    ticker = Honda
+if ticker == "Ford":
+    ticker = Ford
+if ticker == "Stellantis":
+    ticker = Stellantis
+if ticker == "Hyundai":
+    ticker = Hyundai
+if ticker == "Volvo":
+    ticker = Volvo
+if ticker == "Audi":
+    ticker = Audi
+if ticker == "BMW":
+    ticker = BMW
+if ticker == "Mercedes":
+    ticker = Mercedes
 
 
-# Define bitcoin and ethereum and other cryptocurrency abbreviation used by Yahoo Finance.
-Bitcoin = 'BTC-USD'
-GeneralMotors = 'GM'
-Tesla = 'TSLA'
-BTC_Data = yf.Ticker(Bitcoin)
+@st.cache(suppress_st_warning=True) 
+def load_stock_data():
+    stock_data = yf.Ticker(ticker).info
+    return stock_data
 
-GM_Data = yf.Ticker(GeneralMotors)
-TSLA_Data = yf.Ticker(Tesla)
+data = load_stock_data() 
 
+@st.cache(suppress_st_warning=True) 
+def stock_history():
+    stock_history = yf.Ticker(ticker).history(period="YTD" )
+    return stock_history
 
-#for all_tickers.info[["industry"] == ["Auto Manufacturers"]]:
+history = stock_history()
 
-GM_History = GM_Data.history(period="max")
-GM_Price = GM_Data.info['regularMarketPrice']
+@st.cache(suppress_st_warning=True) 
+def load_stock_price():
+    stock_price = yf.Ticker(ticker).info['regularMarketPrice']
+    return stock_price
 
-TSLA_History = TSLA_Data.history(period="max")
-TSLA_Price = TSLA_Data.info['regularMarketPrice']
-
-imageLINK = Image.open(urlopen('https://s2.coinmarketcap.com/static/img/coins/64x64/1975.png'))
-st.image(imageLINK, use_column_width=False)
-
-# GM Stock Price
-st.write(""" ## General Motors Company  """, f"${GM_Price:.2f}")
-image_GM = Image.open('./images/gm.png')
-st.image(image_GM, use_column_width=False)
-st.line_chart(GM_History.Close, use_container_width=True)
-
-GM_sector = GM_Data.info['sector']
-st.write(GM_sector)
-
-# Tesla Stock Price
-st.write(""" ## Tesla, Inc. """, f"${TSLA_Price:.2f}")
-image_TSLA = Image.open('./images/tsla.png')
-st.image(image_TSLA, use_column_width=False)
-st.line_chart(TSLA_History.Close, use_container_width=True)
-
-#TSLA_sector = TSLA_Data.info['sector']
-
+price = load_stock_price()
+current_timestamp = datetime.now()
+st.write(f"### {current_timestamp}")
+st.write(f"## The price of {ticker} in USD is ${price:.2f}")
+st.write("YTD Closing Price")
+st.line_chart(history.Close,use_container_width=True)
+if st.button("More Info"):
+    st.write(data)
 
